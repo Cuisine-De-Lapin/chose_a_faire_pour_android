a8f04b916a706c00a424b5a4d62f1dd87783f5c6
Cuisine-De-Lapin <cuisine_de_lapin@naver.com>
2022-08-16 14:18:15 +0900

    finir niveau 1

--- app/src/main/java/cuisine/de/lapin/choseafaire/MainActivity.kt
+++ app/src/main/java/cuisine/de/lapin/choseafaire/MainActivity.kt
@@ -4,19 +4,37 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.ColumnScope
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyItemScope
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
-import com.google.gson.Gson
+import androidx.compose.ui.unit.sp
+import coil.compose.rememberAsyncImagePainter
+import coil.request.ImageRequest
+import coil.size.Size
+import cuisine.de.lapin.choseafaire.model.FlipDataModel
+import cuisine.de.lapin.choseafaire.network.NetworkProperties
 import cuisine.de.lapin.choseafaire.ui.theme.ChoseAFairePourAndroidTheme
+import cuisine.de.lapin.choseafaire.ui.theme.Purple500
+import cuisine.de.lapin.choseafaire.ui.theme.Purple700
 import cuisine.de.lapin.choseafaire.viewmodel.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.delay
@@ -29,9 +47,6 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         val viewModel: MainViewModel by viewModels()
-
-        viewModel.getMainData()
-        viewModel.getWeatherData()
 
         setContent {
             ChoseAFairePourAndroidTheme {
@@ -49,11 +64,21 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun ShowMain(viewModel: MainViewModel, userName: String) {
+    viewModel.getMainData()
+    viewModel.getWeatherData()
+
     Surface(
         modifier = Modifier.fillMaxSize(),
         color = MaterialTheme.colors.background
     ) {
-        Column {
+        backgroundView(viewModel = viewModel)
+        Column(
+            modifier = Modifier
+                .padding(
+                    horizontal = dimensionResource(id = R.dimen.horizontal_padding),
+                )
+                .padding(top = dimensionResource(id = R.dimen.top_padding)),
+        ) {
             TopUI(userName)
             WeatherInfo(viewModel = viewModel)
             Quote(viewModel = viewModel)
@@ -64,46 +89,101 @@ fun ShowMain(viewModel: MainViewModel, userName: String) {
 }
 
 @Composable
-fun Entrance(viewModel: MainViewModel) {
-    var text by remember {mutableStateOf("")}
-    Row {
-        TextField(
-            value = text,
-            onValueChange = { value -> text = value},
-            label = {Text("Comment vous applez-vous ?")}
-        )
+fun backgroundView(viewModel: MainViewModel) {
+    val mainData by viewModel.mainData.observeAsState()
 
-        Button(onClick = {
-            viewModel.saveLoginName(text)
-        }) {
-            Text("Log In")
+    val painter = rememberAsyncImagePainter(
+        model = ImageRequest.Builder(LocalContext.current)
+            .data("${NetworkProperties.BASE_URL}${mainData?.background ?: ""}")
+            .size(Size.ORIGINAL) // Set the target size to load the image at.
+            .build()
+    )
+
+    Image(
+        painter = painter,
+        contentDescription = stringResource(id = R.string.background_description),
+        modifier = Modifier.fillMaxSize(),
+        contentScale = ContentScale.Crop,
+        alpha = 0.5f
+    )
+}
+
+@Composable
+fun Entrance(viewModel: MainViewModel) {
+    var text by remember { mutableStateOf("") }
+    val focusManager = LocalFocusManager.current
+
+    Box(modifier = Modifier.fillMaxSize()) {
+        Row(
+            modifier = Modifier
+                .align(Alignment.Center)
+                .padding(horizontal = dimensionResource(id = R.dimen.horizontal_padding))
+        ) {
+            TextField(
+                value = text,
+                colors = TextFieldDefaults.textFieldColors(textColor = Purple700),
+                onValueChange = { value -> text = value },
+                label = {
+                    Text(
+                        text = stringResource(id = R.string.label_login),
+                        color = Purple500
+                    )
+                },
+                singleLine = true,
+                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+                keyboardActions = KeyboardActions(
+                    onDone = {
+                        viewModel.saveLoginName(text)
+                        focusManager.clearFocus()
+                    }),
+                modifier = Modifier
+                    .height(dimensionResource(id = R.dimen.login_height))
+                    .weight(2f)
+            )
+
+            Button(
+                onClick = {
+                    viewModel.saveLoginName(text)
+                },
+                modifier = Modifier
+                    .height(dimensionResource(id = R.dimen.login_height))
+                    .weight(1f)
+            ) {
+                Text(stringResource(id = R.string.button_login))
+            }
         }
     }
+
 }
 
 @Composable
 fun ColumnScope.TopUI(userName: String) {
     Row(modifier = Modifier.weight(1f)) {
-       Bienvenue(userName = userName)
-       Time()
+        Bienvenue(userName = userName)
+        Time()
     }
 }
 
 @Composable
-fun Bienvenue(userName: String) {
-    Text(text = "Bonjour, $userName.")
+fun RowScope.Bienvenue(userName: String) {
+    Text(
+        text = stringResource(
+            id = R.string.welcome, userName
+        ),
+        modifier = Modifier.weight(1f)
+    )
 }
 
 @Composable
 fun Time() {
-    var date by remember { mutableStateOf(Date()) } // 2
-    LaunchedEffect(0) { // 3
+    var date by remember { mutableStateOf(Date()) }
+    LaunchedEffect(0) {
         while (true) {
             date = Date()
             delay(1000)
         }
     }
-    
+
     ShowClock(date = date)
 }
 
@@ -116,34 +196,85 @@ fun ShowClock(date: Date) {
 @Composable
 fun ColumnScope.WeatherInfo(viewModel: MainViewModel) {
     val weather by viewModel.weatherData.observeAsState()
-    Text(
-        text = "${weather?.main?.temp} °C, ${weather?.weather?.get(0)?.description}",
-        modifier = Modifier.weight(1f)
-    )
+    Row(
+        modifier = Modifier
+            .weight(1.5f)
+    ) {
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .align(Alignment.CenterVertically)
+        ) {
+            Text(text = weather?.name ?: "")
+            Text(text = weather?.weather?.get(0)?.description ?: "")
+        }
+
+        Text(
+            text = stringResource(id = R.string.temperature, weather?.main?.temp ?: 0f),
+            fontSize = 15.sp,
+            modifier = Modifier.align(Alignment.CenterVertically)
+        )
+    }
 }
 
 @Composable
 fun ColumnScope.Quote(viewModel: MainViewModel) {
     val mainData by viewModel.mainData.observeAsState()
-    Column(modifier = Modifier.weight(2f)) {
-        Text("${mainData?.quote?.quote}")
-        Text("- ${mainData?.quote?.author}")
+    Column(
+        modifier = Modifier
+            .weight(2f)
+    ) {
+        Text(text = mainData?.quote?.quote ?: "")
+        Text(
+            text = stringResource(id = R.string.author_quote, mainData?.quote?.author ?: ""),
+            modifier = Modifier.align(Alignment.End)
+        )
     }
 
 }
 
 @Composable
 fun ColumnScope.ToDoList(viewModel: MainViewModel) {
-    var content by remember {
+    var content by rememberSaveable {
         mutableStateOf("")
     }
-    Column(modifier = Modifier.weight(6f)) {
-        TextField(value = content, onValueChange = { value -> content = value}, label = {Text("Écrivez une chose à faire et appuyez sur Entrée.")})
-        Button(onClick = {
-            viewModel.insert(content)
-        }) {
-            Text(text = "Continuer")
+    val focusManager = LocalFocusManager.current
+
+    Column(modifier = Modifier.weight(8f)) {
+        Row {
+            TextField(
+                value = content,
+                onValueChange = { value -> content = value },
+                singleLine = true,
+                label = {
+                    Text(
+                        text = stringResource(id = R.string.label_todo),
+                        fontSize = 10.sp
+                    )
+                },
+                keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Default),
+                keyboardActions = KeyboardActions(onDone = {
+                    viewModel.insert(content)
+                    focusManager.clearFocus()
+                    content = ""
+
+                }),
+                modifier = Modifier
+                    .weight(1f)
+                    .height(dimensionResource(id = R.dimen.todo_height))
+            )
+            Button(
+                onClick = {
+                    viewModel.insert(content)
+                },
+                modifier = Modifier
+                    .padding(start = dimensionResource(id = R.dimen.button_padding))
+                    .height(dimensionResource(id = R.dimen.todo_height))
+            ) {
+                Text(text = stringResource(id = R.string.button_todo))
+            }
         }
+
         ShowToDos(viewModel)
     }
 
@@ -151,35 +282,58 @@ fun ColumnScope.ToDoList(viewModel: MainViewModel) {
 
 @Composable
 fun ShowToDos(viewModel: MainViewModel) {
-    val flipData = viewModel.todos.observeAsState()
+    val todoData by viewModel.todos.observeAsState()
+    val listState = rememberLazyListState()
 
-    flipData.value?.let {
-        LazyColumn {
-            itemsIndexed(it) { index, item ->
+    todoData?.let {
+        LazyColumn(state = listState) {
+            itemsIndexed(it) { _, item ->
                 Row {
-                    Text(text = "${Gson().toJson(item)}")
-                    Button(onClick = {
-                        viewModel.delete(item)
-                    }) {
-                        Text(text = "X")
+                    Text(
+                        text = item.content,
+                        modifier = Modifier
+                            .weight(1f)
+                            .align(Alignment.CenterVertically)
+                    )
+                    Button(
+                        onClick = {
+                            viewModel.delete(item)
+                        },
+                        modifier = Modifier.align(Alignment.CenterVertically)
+                    ) {
+                        Text(
+                            text = stringResource(id = R.string.button_todo_delete),
+                            modifier = Modifier.align(Alignment.CenterVertically)
+                        )
                     }
                 }
 
             }
         }
     }
+
+    LaunchedEffect(todoData) {
+        listState.scrollToItem(todoData?.size ?: 0)
+    }
 }
 
 
 @Composable
 fun ColumnScope.Buinance(viewModel: MainViewModel) {
-    Column(modifier = Modifier.weight(6f)) {
+    Column(modifier = Modifier.weight(4f)) {
         Row {
-            Button(onClick = { viewModel.doFlip(true) }) {
-                Text("Tête")
+            Button(
+                onClick = { viewModel.doFlip(true) },
+                modifier = Modifier.weight(1f)
+            ) {
+                Text(stringResource(id = R.string.buinance_head))
             }
-            Button(onClick = { viewModel.doFlip(false) }) {
-                Text("Queue")
+            Box(modifier = Modifier.width(dimensionResource(id = R.dimen.button_padding)))
+            Button(
+                onClick = { viewModel.doFlip(false) },
+                modifier = Modifier.weight(1f)
+            ) {
+                Text(stringResource(id = R.string.buinance_tail))
             }
         }
 
@@ -189,15 +343,47 @@ fun ColumnScope.Buinance(viewModel: MainViewModel) {
 
 @Composable
 fun BuinanceLog(viewModel: MainViewModel) {
-    val flipData = viewModel.flipData.observeAsState()
+    val listState = rememberLazyListState()
+    val flipData by viewModel.flipData.observeAsState()
 
-    flipData.value?.let {
-        LazyColumn {
-            itemsIndexed(it) { index, item ->
-                Text(text = "${Gson().toJson(item)}")
+    flipData?.let {
+        LazyColumn(state = listState) {
+            itemsIndexed(it) { _, item ->
+                BuinanceResult(item = item)
             }
         }
     }
+
+    LaunchedEffect(flipData) {
+        listState.scrollToItem(flipData?.size ?: 0)
+    }
+}
+
+@Composable
+fun LazyItemScope.BuinanceResult(item: FlipDataModel) {
+    Text(
+        text = stringResource(
+            id = R.string.buinance_result,
+            getIsHead(isHead = item.isYourHead),
+            getIsHead(isHead = item.isAIHead),
+            stringResource(
+                id = if (item.isSuccess)
+                    R.string.buinance_success
+                else
+                    R.string.buinance_fail
+            )
+        )
+    )
+}
+
+@Composable
+fun getIsHead(isHead: Boolean): String {
+    return stringResource(
+        id = if (isHead)
+            R.string.buinance_head
+        else
+            R.string.buinance_tail
+    )
 }
 
 @Preview(showBackground = true)
--- app/src/main/java/cuisine/de/lapin/choseafaire/ui/theme/Type.kt
+++ app/src/main/java/cuisine/de/lapin/choseafaire/ui/theme/Type.kt
@@ -1,6 +1,7 @@
 package cuisine.de.lapin.choseafaire.ui.theme
 
 import androidx.compose.material.Typography
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
